62cc68d3881b3793b56fd7e0253dfc830b442fdd
fox-feet-slippery <13692929889@163.com>
2024-10-17 23:44:01 +0800

    类和对象2

--- 2024_10_17_test/2024_10_17_test/Date.cpp
+++ 2024_10_17_test/2024_10_17_test/Date.cpp
@@ -193,3 +193,25 @@ int Date::operator-(const Date& d)
 	int day = GetYtoYDay(max._year, min._year);
 	return day + day3;
 }
+
+//int Date::operator-(const Date& d)
+//{
+//	Date max = *this;
+//	Date min = d;
+//	int flag = 1;
+//	if (*this < d)
+//	{
+//		max = d;
+//		min = *this;
+//		flag = -1;
+//	}
+//
+//	int n = 0;
+//	while (min != max)
+//	{
+//		++min;
+//		++n;
+//	}
+//	return n * flag;
+//
+//}
